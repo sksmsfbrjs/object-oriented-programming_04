319cecb2fdcf0b39741eb4b9514f93ac34f68fae
sksmsfbrjs <65989284+sksmsfbrjs@users.noreply.github.com>
2020-09-30 15:22:34 +0900

    20203103-Lab04

---
+++ 20203103-Lab04/src/DoubleTester.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+
+public class DoubleTester {
+	public static void main(String args[]) {
+		double probability = 8.70;
+		double percentage = (100 * probability);
+		System.out.println(Math.round(percentage));
+	}
+}
